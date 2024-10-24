57ed79ef52209ede7be3f8d88f4cad9f8beb7b77
Daniel <mail@nerdfactor.eu>
2024-10-24 19:16:34 +0200

    feat: draw lines DDA feat: draw lines bresenham test: test drawing lines test: test complex form BHT logo

--- AS2/src/index.js
+++ AS2/src/index.js
@@ -1,5 +1,4 @@
 // APP INIT
-import {exampleImageRawData} from './exampleImageRawData.js'; // Import the example image data.
 
 // sets up a canvas with a pixel size feature
 let setupCanvas = function (canvas, width, height, pixelSize) {
@@ -31,6 +30,19 @@ let pixelIndex = function (x, y, framebuffer) {
 
 // most basic function to draw to a framebuffer
 function setPixel(x, y, color, framebuffer) {
+	// check required parameters
+	if(x == null || y == null) {
+		console.log("x or y is null or undefined")
+		return
+	}
+	if (framebuffer == null || framebuffer.width == null) {
+		console.log("framebuffer is null or undefined")
+		return
+	}
+	if (color == null || color.length < 4) {
+		console.log("color is null or undefined")
+		return
+	}
 
 	// EXERCISE 1
 
@@ -57,25 +69,133 @@ function setPixel(x, y, color, framebuffer) {
 	data[offset + 3] = color[3]
 }
 
+/**
+ * Draws a line from the coordinates (x0, y0) to (x1, y1) with the specified color.
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ * @param color
+ * @param framebuffer
+ * @param algorythm
+ */
+function drawLine(x0, y0, x1, y1, color, framebuffer, algorythm = LINE_ALGORITHM) {
+	if (algorythm === 'bresenham') {
+		drawLineBresenham(x0, y0, x1, y1, color, framebuffer)
+	} else if (algorythm === 'dda') {
+		drawLineDDA(x0, y0, x1, y1, color, framebuffer)
+	}
+}
+
+/**
+ * Draws a line using the Bresenham algorithm.
+ * @param startX
+ * @param startY
+ * @param endX
+ * @param endY
+ * @param color
+ * @param framebuffer
+ */
+function drawLineBresenham(startX, startY, endX, endY, color, framebuffer) {
+	const deltaX = Math.abs(endX - startX)
+	const deltaY = Math.abs(endY - startY)
+
+	let signX = startX < endX ? 1 : -1
+	let signY = startY < endY ? 1 : -1
+
+	let decisionParam
+
+	if (deltaX >= deltaY) {
+		decisionParam = 2 * deltaY - deltaX
+
+		while (startX !== endX) {
+			setPixel(startX, startY, color, framebuffer)
+
+			if (decisionParam > 0) {
+				startY += signY
+				decisionParam -= 2 * deltaX
+			}
+
+			startX += signX
+			decisionParam += 2 * deltaY
+		}
+	} else {
+		decisionParam = 2 * deltaX - deltaY
+
+		while (startY !== endY) {
+			setPixel(startX, startY, color, framebuffer)
+
+			if (decisionParam > 0) {
+				startX += signX
+				decisionParam -= 2 * deltaY
+			}
+
+			startY += signY
+			decisionParam += 2 * deltaX
+		}
+	}
+
+	setPixel(endX, endY, color, framebuffer)
+}
+
+/**
+ * Draws a line using the DDA algorithm.
+ * @param startX
+ * @param startY
+ * @param endX
+ * @param endY
+ * @param color
+ * @param framebuffer
+ */
+function drawLineDDA(startX, startY, endX, endY, color, framebuffer) {
+	const deltaX = endX - startX
+	const deltaY = endY - startY
+
+	const steps = Math.max(Math.abs(deltaX), Math.abs(deltaY))
+
+	const xIncrement = deltaX / steps
+	const yIncrement = deltaY / steps
+
+	let x = startX
+	let y = startY
+
+	for (let i = 0; i <= steps; i++) {
+		setPixel(Math.round(x), Math.round(y), color, framebuffer)
+
+		x += xIncrement
+		y += yIncrement
+	}
+}
+
 // draw some pixels
 function renderScene(framebuffer) {
-	let red = [255, 0, 0, 255]
-	let green = [0, 255, 0, 255]
-	let blue = [0, 0, 255, 255]
-	setPixel(1, 1, red, framebuffer)
-	setPixel(10, 10, green, framebuffer)
-	setPixel(10, 1, blue, framebuffer)
+	// let red = [255, 0, 0, 255]
+	// let green = [0, 255, 0, 255]
+	// let blue = [0, 0, 255, 255]
+	// setPixel(1, 1, red, framebuffer)
+	// setPixel(10, 10, green, framebuffer)
+	// setPixel(10, 1, blue, framebuffer)
 
 	// EXERCISE 1
-	testDrawSmallestCoordinates(framebuffer)
-	testDrawLargestCoordinates(framebuffer)
-	testDrawCoordinatesWithinCanvas(framebuffer)
-	testDrawCoordinatesOutsideCanvas(framebuffer)
-	testDrawWithFloatCoordinates(framebuffer)
-
-	drawImageFromPixelArray(exampleImageRawData, 80, 50, 30, 20, framebuffer)
+	// testDrawSmallestCoordinates(framebuffer)
+	// testDrawLargestCoordinates(framebuffer)
+	// testDrawCoordinatesWithinCanvas(framebuffer)
+	// testDrawCoordinatesOutsideCanvas(framebuffer)
+	// testDrawWithFloatCoordinates(framebuffer)
+	//
+	// drawImageFromPixelArray(exampleImageRawData, 80, 50, 30, 20, framebuffer)
 
 	// EXERCISE 1 END
+
+	// EXERCISE 2
+
+	testDrawLineHorizontal(framebuffer)
+	testDrawLineVertical(framebuffer)
+	testDrawLineDiagonal(framebuffer)
+	testDrawLineLogo(framebuffer)
+
+	// EXERCISE 2 END
+
 }
 
 /**
@@ -212,6 +332,81 @@ function testDrawWithFloatCoordinates() {
 }
 
 /**
+ * Test drawing horizontal lines.
+ * Expects two horizontal lines to be drawn next to each other.
+ * @param framebuffer
+ */
+function testDrawLineHorizontal(framebuffer) {
+	let red = [255, 0, 0, 255]
+	drawLine(4, 4, 24, 4, red, framebuffer)
+	let blue = [0, 0, 255, 255]
+	drawLine(24, 6, 4, 6, blue, framebuffer)
+}
+
+/**
+ * Test drawing vertical lines.
+ * Expects two vertical lines to be drawn next to each other.
+ * @param framebuffer
+ */
+function testDrawLineVertical(framebuffer) {
+	let orange = [255, 165, 0, 255]
+	drawLine(4, 8, 4, 28, orange, framebuffer)
+	let green = [0, 255, 0, 255]
+	drawLine(6, 28, 6, 8, green, framebuffer)
+}
+
+/**
+ * Test drawing lines from a central point diagonally every 45 degrees.
+ * Expect lines to be drawn in the form of a star.
+ * @param framebuffer
+ */
+function testDrawLineDiagonal(framebuffer) {
+	let lime = [0, 255, 0, 255]
+	let yellow = [255, 255, 0, 255]
+	let cyan = [0, 255, 255, 255]
+	let purple = [128, 0, 128, 255]
+	let pink = [255, 192, 203, 255]
+	let maroon = [128, 0, 0, 255]
+	let teal = [0, 128, 128, 255]
+	let green = [0, 128, 0, 255]
+
+	drawLine(30, 30, 50, 50, lime, framebuffer)
+	drawLine(30, 30, 50, 30, yellow, framebuffer)
+	drawLine(30, 30, 50, 10, cyan, framebuffer)
+	drawLine(30, 30, 30, 10, purple, framebuffer)
+	drawLine(30, 30, 10, 10, pink, framebuffer)
+	drawLine(30, 30, 10, 30, maroon, framebuffer)
+	drawLine(30, 30, 10, 50, teal, framebuffer)
+	drawLine(30, 30, 30, 50, green, framebuffer)
+}
+
+/**
+ * Test drawing BHT logo using line primitives.
+ * @param framebuffer
+ */
+function testDrawLineLogo(framebuffer) {
+	let black = [0, 0, 0, 255]
+
+	drawLine(55, 20, 55, 32, black, framebuffer);
+	drawLine(55, 20, 60, 20, black, framebuffer);
+	drawLine(60, 20, 62, 22, black, framebuffer);
+	drawLine(62, 22, 62, 24, black, framebuffer);
+	drawLine(62, 24, 60, 26, black, framebuffer);
+	drawLine(60, 26, 55, 26, black, framebuffer);
+	drawLine(60, 26, 62, 28, black, framebuffer);
+	drawLine(55, 32, 60, 32, black, framebuffer);
+
+	drawLine(70, 24, 70, 32, black, framebuffer);
+	drawLine(80, 20, 80, 32, black, framebuffer);
+	drawLine(70, 26, 80, 26, black, framebuffer);
+
+	drawLine(87, 20, 87, 32, black, framebuffer);
+	drawLine(82, 20, 92, 20, black, framebuffer);
+
+	drawLine(58, 38, 74, 16, black, framebuffer);
+}
+
+/**
  * Draws an image provided as a flat array of the rgba color values for each pixel.
  * The image will be drawn at the specified starting coordinates within the canvas.
  * @param pixelArray The flat array of rgba color values for each pixel.
@@ -253,7 +448,8 @@ const CANVAS_WIDTH = 600
 const CANVAS_HEIGHT = 400
 const CANVAS_PIXELSIZE = 5
 const COORDINATE_ROUNDING = 'round' // 'round', 'floor', 'ceil', 'trunc'
-const COORDINATE_CLAMPING = true
+const COORDINATE_CLAMPING = false
+const LINE_ALGORITHM = 'bresenham' // 'bresenham', 'dda'
 
 // get and customize our canvas 
 let canvas = document.getElementById('canvas')
